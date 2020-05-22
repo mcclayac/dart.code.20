935681804d82769e3cd7ccc2a08a7a2ea16b8838
mcclayac <anthony.mcclay@gmail.com>
2020-05-21 23:32:51 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,6 @@
+//code 1.20
+main(List<String> arguments) {
+  String stringWithSingleQuote = 'I\'m a single quote';
+  String stringWithDoubleQuote = "I'm a double quote.";
+  print("Using delimiter in single quote - ${stringWithSingleQuote} and using delimiter in double quote - ${stringWithDoubleQuote}");
+}
