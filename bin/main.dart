2cc3e29ae2970289d340c17ba447057cb6ca8e3b
mcclayac <anthony.mcclay@gmail.com>
2020-05-21 23:40:03 -0400

    Added two more Strings and Printed them

--- bin/main.dart
+++ bin/main.dart
@@ -2,5 +2,12 @@
 main(List<String> arguments) {
   String stringWithSingleQuote = 'I\'m a single quote';
   String stringWithDoubleQuote = "I'm a double quote.";
+
+  String nameWithSingleQuote = 'Tony\'s car is red';
+  String nameWithDoubleQuote = "Tony's car is red";
+
   print("Using delimiter in single quote - ${stringWithSingleQuote} and using delimiter in double quote - ${stringWithDoubleQuote}");
+  print("Here is an example using single  - ${nameWithSingleQuote}");
+  print('Here is an example using Double  - ${nameWithDoubleQuote}');
+
 }
